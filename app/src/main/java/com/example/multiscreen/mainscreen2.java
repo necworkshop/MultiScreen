888c96146a5976d38a2d274ed7292aaade7da57e
shnathan <2012064@nec.edu.in>
2022-09-17 20:30:37 +0530

    Initial commit

--- app/src/main/java/com/example/multiscreen/mainscreen2.java
+++ app/src/main/java/com/example/multiscreen/mainscreen2.java
@@ -1,20 +1,32 @@
 package com.example.multiscreen;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class mainscreen2 extends AppCompatActivity {
 
-
-
-
-
-   private  Button button;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen2);
-        button=findViewById(R.id.)
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @NonNull
+            @Override
+            public void onClick(View v){
+                openActivity2();
+
+            }
+        });
+         public void openActivity2(){
+            Intent intent=new Intent(this,Activity2.class);
+            startActivity(intent);
+        }
     }
 }
