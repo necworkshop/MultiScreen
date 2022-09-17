a451dde7ae40912477fc1cd2a949a0011c4cb0ff
shnathan <2012064@nec.edu.in>
2022-09-17 20:17:38 +0530

    Initial commit

---
+++ app/src/main/java/com/example/multiscreen/mainscreen2.java
@@ -0,0 +1,20 @@
+package com.example.multiscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainscreen2 extends AppCompatActivity {
+
+
+
+
+
+   private  Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainscreen2);
+        button=findViewById(R.id.)
+    }
+}
