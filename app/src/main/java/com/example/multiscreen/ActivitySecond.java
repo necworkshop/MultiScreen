2d924f815493fcc351f3367ff519aef5f1c344d9
Vigneshwaran J <sjvignesh333@gmail.com>
2022-09-17 21:58:46 +0530

    Renamed file and Buttons

--- app/src/main/java/com/example/multiscreen/ActivitySecond.java
+++ app/src/main/java/com/example/multiscreen/ActivitySecond.java
@@ -1,13 +1,10 @@
 package com.example.multiscreen;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class ActivitySecond extends AppCompatActivity {
 
@@ -16,7 +13,7 @@ public class ActivitySecond extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mainscreen2);
+        setContentView(R.layout.second_activity);
         button2 = findViewById(R.id.button2);
         button2.setOnClickListener(new View.OnClickListener() {
 
