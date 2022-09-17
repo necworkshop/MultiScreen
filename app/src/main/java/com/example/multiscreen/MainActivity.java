802956615e542b65df34b0e8a49c569fbc230cc4
shnathan <2012064@nec.edu.in>
2022-09-17 20:43:46 +0530

    second commit

--- app/src/main/java/com/example/multiscreen/MainActivity.java
+++ app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -3,16 +3,31 @@ package com.example.multiscreen;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                openmainscreen2();
+
+            }
+        });
+    }
+        public void openmainscreen2(){
+            Intent intent=new Intent(this,mainscreen2.class);
+            startActivity(intent);
+        }
 
     }
-}
--- app/src/main/java/com/example/multiscreen/mainscreen2.java
+++ app/src/main/java/com/example/multiscreen/mainscreen2.java
@@ -10,23 +10,11 @@ import android.widget.Button;
 
 public class mainscreen2 extends AppCompatActivity {
 
-    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen2);
-        button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @NonNull
-            @Override
-            public void onClick(View v){
-                openActivity2();
 
-            }
-        });
-         public void openActivity2(){
-            Intent intent=new Intent(this,Activity2.class);
-            startActivity(intent);
-        }
     }
 }
