90cfb03fa75c342880590f26ff9405e45bf5cb2e
Vigneshwaran J <sjvignesh333@gmail.com>
2022-09-18 12:50:57 +0530

    StartActivityForResult,setResult Added

--- app/src/main/java/com/example/multiscreen/ActivitySecond.java
+++ app/src/main/java/com/example/multiscreen/ActivitySecond.java
@@ -2,6 +2,8 @@ package com.example.multiscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -21,9 +23,17 @@ public class ActivitySecond extends AppCompatActivity {
             public void onClick(View v) {
                 //Toast.makeText(getApplicationContext(), "shnathan", Toast.LENGTH_SHORT).show();
 
-                finish();
+                open2();
             }
         });
+    }
+    public void open2(){
+//        Intent intent = new Intent(this,MainActivity.class);
+//        startActivity(intent);
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra(MainActivity.RESULT_KEY, "Second Screen Value = " + System.currentTimeMillis());
+        setResult(Activity.RESULT_OK, returnIntent);
+        finish();
     }
 
 
--- app/src/main/java/com/example/multiscreen/MainActivity.java
+++ app/src/main/java/com/example/multiscreen/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.multiscreen;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
@@ -25,10 +27,27 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    static final int LAUNCH_SECOND_ACTIVITY = 1;
+    static final String RESULT_KEY = "result";
         private void openActivitySecond(){
             Intent intent=new Intent(this,ActivitySecond.class);
-            startActivity(intent);
+            startActivityForResult(intent, LAUNCH_SECOND_ACTIVITY);
 
         }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
+        if (requestCode == LAUNCH_SECOND_ACTIVITY) {
+            if(resultCode == Activity.RESULT_OK){
+                String result = data.getStringExtra(MainActivity.RESULT_KEY);
+                Toast.makeText(this, "Result=" + result, Toast.LENGTH_SHORT).show();
+            }
+            if (resultCode == Activity.RESULT_CANCELED) {
+                // Write your code if there's no result
+            }
+        }
     }
+
+
+}
